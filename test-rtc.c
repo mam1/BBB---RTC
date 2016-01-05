afa20b1571c8750455b5d4e1be4cb5758d6494d0
mam1 <mam1@icloud.com>
2016-01-05 15:48:24 -0500

    working demo program

--- test-rtc.c
+++ test-rtc.c
@@ -1,155 +1,172 @@
-#include <errno.h>
-#include <string.h>
+
+/*
+ This software uses a BSD license.
+
+Copyright (c) 2010, Sean Cross / chumby industries
+All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the
+   distribution.  
+ * Neither the name of Sean Cross / chumby industries nor the names
+   of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written
+   permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>		//sleep
-#include <stdint.h>		//uint_8, uint_16, uint_32, etc.
+#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-//#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
 
-#define I2C_BUSS 				"/dev/i2c-1"
-// #define PCF8583_WRITE_ADDRESS 	0xA0 
-// #define PCF8583_READ_ADDRESS  	0xA1
+#define I2C_FILE_NAME "/dev/i2c-1"
+#define USAGE_MESSAGE \
+    "Usage:\n" \
+    "  %s r [addr] [register]   " \
+        "to read value from [register]\n" \
+    "  %s w [addr] [register] [value]   " \
+        "to write a value [value] to register [register]\n" \
+    ""
 
-#define PCF8583_WRITE_ADDRESS 	0x50 
-#define PCF8583_READ_ADDRESS  	0x51
+static int set_i2c_register(int file,
+                            unsigned char addr,
+                            unsigned char reg,
+                            unsigned char value) {
 
-int file;
-char *filename = I2C_BUSS;
+    unsigned char outbuf[2];
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[1];
 
-struct tm
-{
-  int	tm_sec;
-  int	tm_min;
-  int	tm_hour;
-  int	tm_mday;
-  int	tm_mon;
-  int	tm_year;
-  int	tm_wday;
-  int	tm_yday;
-  int	tm_isdst;
-} rtc_time;
+    messages[0].addr  = addr;
+    messages[0].flags = 0;
+    messages[0].len   = sizeof(outbuf);
+    messages[0].buf   = outbuf;
 
-//---------------------------------------------- 
-// This function converts an 8 bit binary value 
-// to an 8 bit BCD value. 
-// The input range must be from 0 to 99. 
+    /* The first byte indicates which register we'll write */
+    outbuf[0] = reg;
 
-uint8_t  bin2bcd(uint8_t  value) 
-{ 
-char retval; 
+    /* 
+     * The second byte indicates the value to write.  Note that for many
+     * devices, we can write multiple, sequential registers at once by
+     * simply making outbuf bigger.
+     */
+    outbuf[1] = value;
 
-retval = 0; 
+    /* Transfer the i2c packets to the kernel and verify it worked */
+    packets.msgs  = messages;
+    packets.nmsgs = 1;
+    if(ioctl(file, I2C_RDWR, &packets) < 0) {
+        perror("Unable to send data");
+        return 1;
+    }
 
-while(1) 
-  { 
-   // Get the tens digit by doing multiple subtraction 
-   // of 10 from the binary value. 
-   if(value >= 10) 
-     { 
-      value -= 10; 
-      retval += 0x10; 
-     } 
-   else // Get the ones digit by adding the remainder. 
-     { 
-      retval += value; 
-      break; 
-     } 
-   } 
-
-return(retval); 
-} 
-
-//---------------------------------------------- 
-// This function converts an 8 bit BCD value to 
-// an 8 bit binary value. 
-// The input range must be from 00 to 99. 
-
-char bcd2bin(char bcd_value) 
-{ 
-char temp; 
-
-temp = bcd_value; 
-
-// Shifting the upper digit right by 1 is 
-// the same as multiplying it by 8. 
-temp >>= 1; 
-
-// Isolate the bits for the upper digit. 
-temp &= 0x78; 
-
-// Now return: (Tens * 8) + (Tens * 2) + Ones 
-return(temp + (temp >> 2) + (bcd_value & 0x0f)); 
-
-} 
-
-//---------------------------------------------- 
-// This function opens an i2c buss.  
-//  
-
-void i2c_open(void){
-	char *bussname = I2C_BUSS;
-	if((file = open(bussname, O_RDWR)) < 0) {
-	    /* ERROR HANDLING: you can check errno to see what went wrong */
-	    perror("Failed to open the i2c bus");
-	    exit(1);
-	}
-
-	return;
+    return 0;
 }
 
 
-int main(void) {
+static int get_i2c_register(int file,
+                            unsigned char addr,
+                            unsigned char reg,
+                            unsigned char *val) {
+    unsigned char inbuf, outbuf;
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[2];
 
-	printf("\n\ni2c test program\n\n");
-	i2c_open();
-	printf("  i2c open worked\n");
-	int addr = PCF8583_READ_ADDRESS;     // The I2C address of the device
-	printf("i2c device addres set to %x\n",addr);
-	if (ioctl(file, I2C_SLAVE, addr) < 0) {
-	    printf("Failed to acquire bus access and/or talk to slave.\n");
-	    /* ERROR HANDLING; you can check errno to see what went wrong */
-	    exit(1);
-	}
-	printf("  communication initiated\nstart read\n");
+    /*
+     * In order to read a register, we first do a "dummy write" by writing
+     * 0 bytes to the register we want to read from.  This is similar to
+     * the packet in set_i2c_register, except it's 1 byte rather than 2.
+     */
+    outbuf = reg;
+    messages[0].addr  = addr;
+    messages[0].flags = 0;
+    messages[0].len   = sizeof(outbuf);
+    messages[0].buf   = &outbuf;
 
-	unsigned char buf[9] = {0};
-	int i;
-	 
-  for(i = 0; i<20; i++){
-	// Using I2C Read
-	if (read(file,buf,9) != 9) {
-	 	/* ERROR HANDLING: i2c transaction failed */
-	    printf("Failed to read from the i2c bus: %s.\n", strerror(errno));
-	    printf("\n\n");
-	    } 
-	 else {
-	  	// printf("  read worked\n");
-	 // for(i=0;i<10;i++)
-	 // 	printf("    byte %i <%2x>\n",i,buf[i]);
-	
-		 rtc_time.tm_sec = bcd2bin(buf[2] & 0x7f);
-	  	 rtc_time.tm_min = bcd2bin(buf[3] & 0x7f);
-	  	 rtc_time.tm_hour = bcd2bin(buf[4] & 0x3f);
-	  	 rtc_time.tm_mday = bcd2bin(buf[5] & 0x3f);
-	  	 rtc_time.tm_mon = bcd2bin(buf[7] & 0x1f) - 1;
-	  	 rtc_time.tm_year = bcd2bin(buf[8]) + 100;
-	  	 rtc_time.tm_wday = bcd2bin(buf[6] & 0x3);
-	  	 printf("time %02i:%02i:%02i \t date %02i/%02i/%02i  \t dow %i\n",
-	  	 	rtc_time.tm_hour, 
-	  	 	rtc_time.tm_min, 
-	  	 	rtc_time.tm_sec, 
-	  	 	rtc_time.tm_mday, 
-	  	 	rtc_time.tm_mon, 
-	  	 	rtc_time.tm_year, 
-	  	 	rtc_time.tm_wday);
-	  	 sleep(1);
-	}
+    /* The data will get returned in this structure */
+    messages[1].addr  = addr;
+    messages[1].flags = I2C_M_RD/* | I2C_M_NOSTART*/;
+    messages[1].len   = sizeof(inbuf);
+    messages[1].buf   = &inbuf;
+
+    /* Send the request to the kernel and get the result back */
+    packets.msgs      = messages;
+    packets.nmsgs     = 2;
+    if(ioctl(file, I2C_RDWR, &packets) < 0) {
+        perror("Unable to send data");
+        return 1;
+    }
+    *val = inbuf;
+
+    return 0;
 }
-	return 0;
+
+
+int main(int argc, char **argv) {
+    int i2c_file;
+
+    // Open a connection to the I2C userspace control file.
+    if ((i2c_file = open(I2C_FILE_NAME, O_RDWR)) < 0) {
+        perror("Unable to open i2c control file");
+        exit(1);
+    }
+
+
+    if(argc > 3 && !strcmp(argv[1], "r")) {
+        int addr = strtol(argv[2], NULL, 0);
+        int reg = strtol(argv[3], NULL, 0);
+        unsigned char value;
+        if(get_i2c_register(i2c_file, addr, reg, &value)) {
+            printf("Unable to get register!\n");
+        }
+        else {
+            printf("Register %d: %d (%x)\n", reg, (int)value, (int)value);
+        }
+    }
+    else if(argc > 4 && !strcmp(argv[1], "w")) {
+        int addr = strtol(argv[2], NULL, 0);
+        int reg = strtol(argv[3], NULL, 0);
+        int value = strtol(argv[4], NULL, 0);
+        if(set_i2c_register(i2c_file, addr, reg, value)) {
+            printf("Unable to get register!\n");
+        }
+        else {
+            printf("Set register %x: %d (%x)\n", reg, value, value);
+        }
+    }
+    else {
+        fprintf(stderr, USAGE_MESSAGE, argv[0], argv[0]);
+    }
+
+
+    close(i2c_file);
+
+
+    return 0;
 }
